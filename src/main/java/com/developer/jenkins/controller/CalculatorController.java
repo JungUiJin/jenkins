3838b2c4843a0b2524d757315fe9c34c75126248
JungUiJin <yealkki38@gmail.com>
2024-11-05 11:32:22 +0900

    test : webhook test

--- src/main/java/com/developer/jenkins/controller/CalculatorController.java
+++ src/main/java/com/developer/jenkins/controller/CalculatorController.java
@@ -17,7 +17,7 @@ public class CalculatorController {
 
     @GetMapping("/health")
     public String healthCheck() {
-        return "I'm alive";
+        return "pipe line test check";
     }
 
     @GetMapping("/plus")
